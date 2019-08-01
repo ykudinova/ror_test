9b8d917a0e4676a3d019343b511512905429a826
ykudinova <jane.tabby@gmail.com>
2019-08-01 17:11:40 +1200

    added a worker to get stock prices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   #GetPricesJob.set(wait: 5.minute).perform_later()
-  GetPricesJob.perform_now()
+  #GetPricesJob.perform_now()
+  PriceWorker.perform_async()
 end
---
+++ app/workers/price_worker.rb
@@ -0,0 +1,44 @@
+class PriceWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    # Do something
+    key = 'A0FUVVLFSCVK26JB'
+
+    users = User.all
+
+    users.each do |user|
+      lists = StockList.all.where(user_id: user.id)
+      lists.each do |list|
+        stocks = Stock.all.where(stock_list_id: list.id)
+        stocks.each do |stock|
+          url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stock.symbol+'&apikey='+key
+          uri = URI(url)
+          response = JSON.parse(Net::HTTP.get(uri))
+
+          if(response)
+            record = Record.new
+            if(response['Global Quote'])
+              record.date = DateTime.now
+              record.price = response['Global Quote']['05. price']
+              record.note = ''
+              record.stock_id = stock.id
+            elsif(response['Note'])
+              record.date = DateTime.now
+              record.price = 0.0
+              record.note = response['Note']
+              record.stock_id = stock.id
+            else
+              record.date = DateTime.now
+              record.price = 0.0
+              record.note = 'Unexpected response from Alpha Vantage'
+              record.stock_id = stock.id
+            end
+            record.save
+          end
+
+        end
+      end
+    end
+  end
+end
